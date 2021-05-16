fd52155ea70da1046fa16a91eadb16846f4b7728
Sakshi Kamal <54491864+sakshikamal@users.noreply.github.com>
2021-05-16 21:37:30 +0530

    Update model.py

--- yolo3/model.py
+++ yolo3/model.py
@@ -5,7 +5,7 @@ from functools import wraps
 import numpy as np
 import tensorflow as tf
 
-tf.python.control_flow_ops = tf
+#tf.python.control_flow_ops = tf
 
 from keras import backend as K
 from keras.layers import Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
@@ -394,7 +394,7 @@ def yolo_loss(args, anchors, num_classes, ignore_thresh=.5, print_loss=False):
             best_iou = K.max(iou, axis=-1)
             ignore_mask = ignore_mask.write(b, K.cast(best_iou<ignore_thresh, K.dtype(true_box)))
             return b+1, ignore_mask
-        _, ignore_mask = tf.control_flow_ops.while_loop(lambda b,*args: b<m, loop_body, [0, ignore_mask])
+        _, ignore_mask = K.control_flow_ops.while_loop(lambda b,*args: b<m, loop_body, [0, ignore_mask])
         ignore_mask = ignore_mask.stack()
         ignore_mask = K.expand_dims(ignore_mask, -1)
 
